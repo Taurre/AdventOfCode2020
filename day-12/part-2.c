5f989a40a3095c899a12eefebea0465908801c1a
Taurre <iso9899-taurre@yahoo.fr>
2020-12-12 12:19:51 +0100

    Remove calls to position_print().

--- day-12/part-2.c
+++ day-12/part-2.c
@@ -12,24 +12,10 @@ main(void)
 	char action;
 	long unit;
 	
-	printf("Waypoint:\n");
-	position_print(&waypoint);
-	printf("Ship position:\n");
-	position_print(&position);
-
 	while (scanf("%c%ld", &action, &unit) == 2) {
-		printf("Applying %c %ld:\n", action, unit);
 		position_update_with_waypoint(&position, &waypoint, action, unit);
-		printf("Waypoint:\n");
-		position_print(&waypoint);
-		printf("Ship position:\n");
-		position_print(&position);
 		scanf("%*c");
 	}
 
-	printf("Waypoint:\n");
-	position_print(&waypoint);
-	printf("Ship position:\n");
-	position_print(&position);
 	printf("Part 2: %ld\n", manathan_distance(&position));
 }

644d9ce80fe8307690c16186684b49c8d164ac18
Taurre <iso9899-taurre@yahoo.fr>
2020-12-13 12:10:03 +0100

    Add day 13 part 1.

---
+++ day-13/part-1.c
@@ -0,0 +1,53 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct bus {
+	long id;
+	long wait;
+};
+
+
+void
+fatal(char const *message)
+{
+	if (errno)
+		perror(message);
+	else
+		fprintf(stderr, "%s\n", message);
+
+	exit(EXIT_FAILURE);
+}
+
+
+int
+main(void)
+{
+	long start;
+
+	if (scanf("%ld\n", &start) != 1)
+		fatal("scanf");
+
+	struct bus bus = { .wait = start };
+	long id;
+
+	while (scanf("%ld", &id) == 1) {
+		if (id - start % id < bus.wait) {
+			bus.wait = id - start % id;
+			bus.id = id;
+		}
+
+		int c;
+
+		while (c = getchar(), c != EOF && c != '\n')
+			if (isdigit(c)) {
+				ungetc(c, stdin);
+				break;
+			}
+	}
+
+	printf("Part 1: %ld\n", bus.id * bus.wait);
+	return 0;
+}
